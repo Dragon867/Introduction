eeac2d4949c23dbd200d3cb33165f44c93acf047
Dragon867 <hamza.jadoon2007@gmail.com>
2024-09-12 23:52:51 +0500

    Add files via upload

---
+++ Intro.py
@@ -0,0 +1,5 @@
+print("My name is Hamza Jadoon and I belong fro Pakistan.")
+print("I am in 1st year, I have 2 siblings.")
+print("My favourite color is blue.")
+print("My favourite game is Football.")
+print("Till now I have a very good experience with codingal.")
